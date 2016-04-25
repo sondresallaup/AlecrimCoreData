2f4a58e96ec871d35509652dba1bbaec3fb84f23
Sondre Sallaup <sondre@sallaup.com>
2016-04-25 14:24:03 +0200

    nh

--- Source/AlecrimCoreData/Core/Structs/DataContextOptions.swift
+++ Source/AlecrimCoreData/Core/Structs/DataContextOptions.swift
@@ -120,7 +120,7 @@ extension NSBundle {
     /// This variable is used to guess a managedObjectModelName.
     /// The provided kCFBundleNameKey we are using to determine the name will include spaces, whereas managed object model name uses underscores in place of spaces by default - hence why we are replacing " " with "_" here
     private var inferredManagedObjectModelName: String? {
-        return "Toktok_Kl_r"
+        return "Toktok_Kl_rP"
 //        return (self.infoDictionary?[String(kCFBundleNameKey)] as? String)?.stringByReplacingOccurrencesOfString(" ", withString: "_")
     }
     
